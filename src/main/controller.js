40b41091d71fb4e51b0506f241103bfa4ec9d40e
wovvan <efanov1993@gmail.com>
2016-04-09 21:03:37 +0300

    fix css

--- src/main/controller.js
+++ src/main/controller.js
@@ -2,6 +2,9 @@ import angular from 'angular'
 import './style.less'
 export default /*@ngInject*/ function() {
   var vm = this;
+  document.getElementById("c").width = "1200";
+  document.getElementById("c").height ="700";
   vm.canvas = new fabric.Canvas('c');
   fabric.Object.prototype.transparentCorners = false;
+
 }
